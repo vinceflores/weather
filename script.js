5c34163a131fe9f8ba1739e4f8439e7a65d065d7
Vince Gabriel Flores <vincegabriel.flores@gmail.com>
2023-04-08 15:40:29 -0400

    first

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+
+    
+    const container = document.querySelector("[data-container]");
+    const input = document.querySelector("[data-input]") ; 
+    const btn = document.getElementById('btn'); 
+
+    let cityName  ="Toronto"; // TODO change to current location city
+    let url = 'http://api.weatherapi.com/v1/current.json?key=0cbb7e4fc1764d81ae3154623230704&q=' + cityName +'&aqi=no';
+    
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      let hello = document.getElementById('hello'); 
+      let city = document.getElementById('city');
+      let currentTemperature = data.current.temp_c;
+      console.log(`The current temperature is ${currentTemperature} degrees Celsius.`);
+      hello.textContent = currentTemperature + '˚';
+      city.textContent = data.location.name;
+    })
+    .catch(error => console.error(error));
+    
+    function handleClick(){
+      cityName =document.getElementById('inp').value;
+      document.getElementById('inp').value = "";
+      let url = 'http://api.weatherapi.com/v1/current.json?key=0cbb7e4fc1764d81ae3154623230704&q=' + cityName +'&aqi=no';
+      
+      fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        let hello = document.getElementById('hello'); 
+        let city = document.getElementById('city');
+        let currentTemperature = data.current.temp_c;
+        console.log(`The current temperature is ${currentTemperature} degrees Celsius.`);
+        hello.textContent = currentTemperature + '˚';
+        city.textContent = data.location.name;
+      })
+      .catch(error => console.error(error));
+    
+    }
+
+     
+
+
+
